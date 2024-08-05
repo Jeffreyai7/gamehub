d7a643a4e326cbd877116b6f8135eb65030cdc5a
jeffreyai7 <jirukeh@gmail.com>
2024-08-05 12:56:14 +0100

    Refactor Game grid

--- src/componenets/GameGrid.tsx
+++ src/componenets/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames, {} from '../hooks/useGames';
+import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton'
 import GameCardContainer from './GameCardContainer';
@@ -17,9 +17,10 @@ function GameGrid({gameQuery}: Props) {
 
     const skeletons = [1, 2, 3, 4, 5, 6];
     
+    if (error) return <Text color={"red"} >{error}</Text>
+
+
     return (
-        error ? <Text color={"red"} >{error}</Text> 
-        : 
         <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4 }} marginTop={2} spacing={10} >
            {
             isLoading && skeletons.map(skeleton => {
